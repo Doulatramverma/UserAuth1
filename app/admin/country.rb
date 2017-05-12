cb6982d2b4706d852c0a857229c3563952e677c9
bittern <bittern@EMAIL.com>
2017-05-12 16:37:41 +0530

    modify

--- app/admin/country.rb
+++ app/admin/country.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Country do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
- permit_params :country_name, :country_code, :attributes, :on, :model
+# permit_params :list, :of, :attributes, :on, :model
 #
 # or
 #
